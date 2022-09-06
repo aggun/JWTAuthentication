b62410d00c0a4550a1c42be768bb879cbf2c3b6a
Nebil Aggun <aggun1903@gmail.com>
2022-09-06 15:15:13 +0300

    Add project files.

---
+++ PycApi/StartUpExtension/ExtensionService.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+using PycApi.Mapper;
+using PycApi.Service;
+
+namespace PycApi.StartUpExtension
+{
+    public static class ExtensionService
+    {
+        public static void AddServices(this IServiceCollection services)
+        {
+            // services 
+            services.AddScoped<IAccountService, AccountService>();
+            services.AddScoped<ITokenService, TokenService>();
+
+            // mapper
+            var mapperConfig = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new MappingProfile());
+            });
+            services.AddSingleton(mapperConfig.CreateMapper());
+        }
+    }
+}
